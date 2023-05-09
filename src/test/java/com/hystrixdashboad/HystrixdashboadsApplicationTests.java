264f4c10318b458d7f4abeb32a7e83c0bd415a91
Nana-1-dev <132481925+Nana-1-dev@users.noreply.github.com>
2023-05-09 07:43:55 +0100

    Initial commit

---
+++ src/test/java/com/hystrixdashboad/HystrixdashboadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hystrixdashboad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixdashboadsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
